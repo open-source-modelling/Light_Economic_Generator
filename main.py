17bf7310b663b4e3367a9bb05c9a84b81d03d08d
Gregor <38689034+GregorFabjan@users.noreply.github.com>
2024-03-31 19:23:29 +0200

    Added Vasicek, made them into functions

--- main.py
+++ main.py
@@ -1,70 +1,19 @@
-import numpy as np
 import pandas as pd
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-from term_structure import *
-from main_calculation import mainCalculation
-from HW_run import *
-
-# Zero coupon bond prices calculated using the assumed term structure
-P0t = lambda t: P0t_f(t, m_obs, Qb, ufr, alpha)
-
+from main_BS import Run_Black_Sholes
+from main_V import Run_Vasicek
+from main_HW import Run_Hull_White
 
 param_raw = pd.read_csv("Parameters.csv", sep=',', index_col=0)
 
-asset_id = 11
-
-selected_param_file = param_raw["selected_param_file"][asset_id]
-selected_curves_file = param_raw["selected_curves_file"][asset_id]
-country = param_raw["Country"][asset_id]
-
-NoOfPaths = param_raw["NoOfPaths"][asset_id] # Number of stochastic scenarios
-NoOfSteps = param_raw["NoOfSteps"][asset_id] # Number of equidistand discrete modelling points (50*12 = 600)
-T = param_raw["T"][asset_id]                 # Time horizon in years (A time horizon of 50 years; T=50)
-a =  param_raw["a"][asset_id]                # Hull-White mean reversion parameter a
-sigma = param_raw["sigma"][asset_id]         # Hull-White volatility parameter sigma
-epsilon =  param_raw["epsilon"][asset_id]     # Incremental distance used to calculate for numerical approximation
-                 # of for example the instantaneous spot rate (Ex. 0.01 will use an interval 
-                 # of 0.01 as a discreete approximation for a derivative)
-
-
-param_raw = pd.read_csv(selected_param_file, sep=',', index_col=0)
-
-maturities_country_raw = param_raw.loc[:,country+"_Maturities"].iloc[6:]
-param_country_raw = param_raw.loc[:,country + "_Values"].iloc[6:]
-extra_param = param_raw.loc[:,country + "_Values"].iloc[:6]
-
-relevant_positions = pd.notna(maturities_country_raw.values)
-maturities_country = maturities_country_raw.iloc[relevant_positions]
-Qb = param_country_raw.iloc[relevant_positions]
-curve_raw = pd.read_csv(selected_curves_file, sep=',',index_col=0)
-curve_country = curve_raw.loc[:,country]
-
-# Maturity of observations:
-m_obs = np.transpose(np.array(maturities_country.values))
-
-# Ultimate froward rate ufr represents the rate to which the rate curve will 
-# converge as time increases:
-ufr = extra_param.iloc[3]/100
-
-# Convergence speed parameter alpha controls the speed at which the curve 
-# converges towards the ufr from the last liquid point:
-alpha = extra_param.iloc[4]
-
-# For which maturities do we want the SW algorithm to calculate the rates. 
-# In this case, for every year up to 150:
-m_target = np.transpose(np.arange(1,151)) 
-
-# Qb calibration vector published by EIOPA for the curve calibration:
-Qb = np.transpose(np.array(Qb.values))
-
-# Final comparison
-[t, P, implied_term_structure, M] = mainCalculation(NoOfPaths, NoOfSteps, T, a, sigma, P0t, epsilon)
-
-out = pd.DataFrame(data = M,columns=t)
-
-print(out)
-
-
-
-
+for asset_id in param_raw.index:
+    if param_raw["model"][asset_id] == "HW":
+        print("HW")
+        print(Run_Hull_White(asset_id=asset_id))        
+    elif param_raw["model"][asset_id] == "BS":
+        print("BS")
+        print(Run_Black_Sholes(asset_id=asset_id))
+    elif param_raw["model"][asset_id] == "V":
+        print("V")
+        print(Run_Vasicek(asset_id=asset_id))
+    else:
+        print("Model not available")
--- main_V.py
+++ main_V.py
@@ -2,35 +2,7 @@ import numpy as np
 import pandas as pd
 from term_structure import *
 
-def HW_theta(a: float, sigma: float, P0t: callable, eps: float) -> callable:
-    """
-    Calculates the theta value for the Hull-White model 
-    using a numeric approximation of the instantaneous forward rate 
-    and the spot rate.
-
-    Args:
-        a (float): Mean reversion rate parameter a.
-        sigma (float): Volatility parameter sigma.
-        P0t (function handle): Function that calculates the price of a
-            zero-coupon bond as a function of time.
-        eps (float): Increment of time used in the numeric calculation of the 
-            derivative of the instantaneous forward rate.
-
-    Returns:
-        theta (function): Function that returns the parameter theta of 
-            Hull-White model at the time t implied by the calibration.
-    """
-    
-    def theta(t:float)->float:
-        insta_forward_term = (f0t(t+eps, P0t, eps) 
-                                         -f0t(t-eps,P0t,eps))/(2.0*eps)
-                                         
-        forward_term = a*f0t(t, P0t, eps)
-        variance_term = sigma**2/(2.0*a)*(1.0-np.exp(-2.0*a*t))
-        return insta_forward_term + forward_term + variance_term
-    return theta
-
-def Paths(NoOfPaths, NoOfSteps, T, P0t, a, sigma, epsilon):
+def Paths_V(NoOfPaths, NoOfSteps, T, P0t, mu, sigma, gamma, epsilon):
     """
     Simulates a series of stochastic interest rate paths using the Hull-White model.
 
@@ -68,7 +40,7 @@ def Paths(NoOfPaths, NoOfSteps, T, P0t, a, sigma, epsilon):
     
     # Calculation of theta = 1/a * partial derivative of f(0,t) w.r.t. t 
     # + f(0,t) + sigma^2/(2 a^2)* (1-exp(-2*a*t)).
-    theta = HW_theta(a, sigma, P0t, epsilon)
+    #theta = HW_theta(a, sigma, P0t, epsilon)
     
     # Generate the single source of random noise.
     Z = np.random.normal(0.0, 1.0, [NoOfPaths, NoOfSteps])
@@ -96,11 +68,15 @@ def Paths(NoOfPaths, NoOfSteps, T, P0t, a, sigma, epsilon):
             
         # Apply the Euler-Maruyama discretisation scheme for the Hull-White model
         # at each time increment.
-        W[:, iTime] = W[:, iTime-1] + np.power(dt, 0.5)*Z[:, iTime-1] 
-        noise_term = sigma* (W[:, iTime]-W[:, iTime-1])
-        rate_term = (theta(time[iTime-1])-a*R[:, iTime-1])*dt
-        R[:, iTime] = R[:, iTime-1] + rate_term + noise_term
-    
+        var_term = sigma**2 /(2*gamma)*(1-np.exp(-2*gamma*dt))
+        
+        W[:, iTime] = W[:, iTime-1] + var_term*Z[:, iTime-1] 
+        rate_term = (1-np.exp(-gamma*dt))*mu
+        noise_term = np.exp(-gamma * dt) 
+
+        R[:, iTime] = rate_term + noise_term * R[:, iTime-1] + (W[:, iTime]-W[:, iTime-1])
+
+
     # Vectorized numeric integration using the Euler integration method .
     M = np.exp(-0.5 * (R[:, :-1] + R[:, 1:]) * dt) 
     M = np.insert(M, 0, 1, axis=1).cumprod(axis=1)
@@ -112,7 +88,7 @@ def Paths(NoOfPaths, NoOfSteps, T, P0t, a, sigma, epsilon):
     return paths
 
 
-def mainCalculation(NoOfPaths, NoOfSteps, T, a, sigma, P0t, epsilon):
+def mainCalculation_V(NoOfPaths, NoOfSteps, T, mu, sigma, gamma, P0t, epsilon):
     """
     Calculates and plots the prices of zero-coupon bonds (ZCB) calculated 
     using the Hull-White model`s analytical formula and the Monte Carlo simulation.
@@ -134,7 +110,7 @@ def mainCalculation(NoOfPaths, NoOfSteps, T, a, sigma, P0t, epsilon):
         implied_term_structure XXX: term structure provided as input into the HW simulation.
     """
  
-    paths = Paths(NoOfPaths, NoOfSteps, T, P0t, a, sigma, epsilon)
+    paths = Paths_V(NoOfPaths, NoOfSteps, T, P0t, mu, sigma, gamma, epsilon)
     M = paths["M"]
     t = paths["time"]
     implied_term_structure = P0t(t)
@@ -144,3 +120,71 @@ def mainCalculation(NoOfPaths, NoOfSteps, T, a, sigma, P0t, epsilon):
         P[i] = np.mean(M[:, i])
     
     return [t, P, implied_term_structure, M]
+
+
+asset_id = 33
+
+def Run_Vasicek(asset_id):
+    P0t = lambda t: P0t_f(t, m_obs, Qb, ufr, alpha)
+
+    param_raw = pd.read_csv("Parameters.csv", sep=',', index_col=0)
+
+    selected_param_file = param_raw["selected_param_file"][asset_id]
+    selected_curves_file = param_raw["selected_curves_file"][asset_id]
+    country = param_raw["Country"][asset_id]
+
+    NoOfPaths = param_raw["NoOfPaths"][asset_id] # Number of stochastic scenarios
+    NoOfSteps = param_raw["NoOfSteps"][asset_id] # Number of equidistand discrete modelling points (50*12 = 600)
+    T = param_raw["T"][asset_id]                 # Time horizon in years (A time horizon of 50 years; T=50)
+    mu =  param_raw["mu"][asset_id]                # Hull-White mean reversion parameter a
+    sigma = param_raw["sigma"][asset_id]         # Hull-White volatility parameter sigma
+    gamma = param_raw["gamma"][asset_id]         # Hull-White volatility parameter sigma
+    epsilon =  param_raw["epsilon"][asset_id]     # Incremental distance used to calculate for numerical approximation
+                    # of for example the instantaneous spot rate (Ex. 0.01 will use an interval 
+                    # of 0.01 as a discreete approximation for a derivative)
+
+    param_raw = pd.read_csv(selected_param_file, sep=',', index_col=0)
+
+    maturities_country_raw = param_raw.loc[:,country+"_Maturities"].iloc[6:]
+    param_country_raw = param_raw.loc[:,country + "_Values"].iloc[6:]
+    extra_param = param_raw.loc[:,country + "_Values"].iloc[:6]
+
+    relevant_positions = pd.notna(maturities_country_raw.values)
+    maturities_country = maturities_country_raw.iloc[relevant_positions]
+    Qb = param_country_raw.iloc[relevant_positions]
+    curve_raw = pd.read_csv(selected_curves_file, sep=',',index_col=0)
+    curve_country = curve_raw.loc[:,country]
+
+    # Maturity of observations:
+    m_obs = np.transpose(np.array(maturities_country.values))
+
+    # Ultimate froward rate ufr represents the rate to which the rate curve will 
+    # converge as time increases:
+    ufr = extra_param.iloc[3]/100
+
+    # Convergence speed parameter alpha controls the speed at which the curve 
+    # converges towards the ufr from the last liquid point:
+    alpha = extra_param.iloc[4]
+
+    # For which maturities do we want the SW algorithm to calculate the rates. 
+    # In this case, for every year up to 150:
+    m_target = np.transpose(np.arange(1,151)) 
+
+    # Qb calibration vector published by EIOPA for the curve calibration:
+    Qb = np.transpose(np.array(Qb.values))
+
+    # Final comparison
+    [t, P, implied_term_structure, M] = mainCalculation_V(NoOfPaths, NoOfSteps, T, mu, sigma, gamma, P0t, epsilon)
+
+    run_name = "V-"+str(asset_id)
+
+    multi_index_list = []
+    for scenario in list(range(0,NoOfPaths)):
+        multi_index_list.append((run_name,scenario))
+
+    multi_index = pd.MultiIndex.from_tuples(multi_index_list, names=('Run', 'Scenario_number'))
+    out = pd.DataFrame(data = M,columns=t,index=multi_index)
+
+    return out
+
+print(Run_Vasicek(33))
