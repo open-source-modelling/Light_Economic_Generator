9916afa7f9667ca25073fa318419582c74bc9883
Open Source Modelling <95974474+open-source-modelling@users.noreply.github.com>
2024-04-01 21:41:17 +0200

    Removed some code that was artifcat

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
-from main_BS import Run_Black_Sholes
-from main_V import Run_Vasicek
-from main_HW import Run_Hull_White
+from BlackSholes import Run_Black_Sholes
+from Vasicek import Run_Vasicek
+from HullWhite import Run_Hull_White
 
 param_raw = pd.read_csv("Parameters.csv", sep=',', index_col=0)
 
@@ -10,16 +10,13 @@ combined_run = []
 
 for asset_id in param_raw.index:
     if param_raw["model"][asset_id] == "HW":
-        print("HW")
         run = Run_Hull_White(asset_id=asset_id)        
     elif param_raw["model"][asset_id] == "BS":
-        print("BS")
         run = Run_Black_Sholes(asset_id=asset_id)
     elif param_raw["model"][asset_id] == "V":
-        print("V")
         run = Run_Vasicek(asset_id=asset_id)
     else:
-        print("Model not available")
+        raise ValueError("Model type not available")
 
     if isinstance(combined_run,pd.DataFrame):
         combined_run = pd.concat([combined_run,run])
